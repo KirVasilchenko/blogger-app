f32d0e657ca14c0290171bdfbc4ffed1d4cfa2b8
KirVasilchenko <uzzer-13@mail.ru>
2021-05-28 08:19:39 +0300

    Initial commit

---
+++ src/main/java/ru/mininuniver/pimz16/bloggerapp/BloggerAppApplication.java
@@ -0,0 +1,13 @@
+package ru.mininuniver.pimz16.bloggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mininuniver/pimz16/bloggerapp/BloggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mininuniver.pimz16.bloggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
